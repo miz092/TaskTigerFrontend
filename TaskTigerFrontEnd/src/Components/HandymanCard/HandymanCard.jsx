1a34b0e7a166c0ebf36977768ee06f70a1303b5e
fulopdenes <denes.fulopp@gmail.com>
2023-03-03 03:48:43 +0100

    add handymanCard Component settings.

--- TaskTigerFrontEnd/src/Components/HandymanCard/HandymanCard.jsx
+++ TaskTigerFrontEnd/src/Components/HandymanCard/HandymanCard.jsx
@@ -2,25 +2,23 @@ import React from "react";
 import "./HandymanCard.css";
 import handymanImage from "../../Images/handyman-janos.jpg"
 
-export default function HandymanCard() {
-  return (
-    <div className="handyman-card">
-      <img
-        src={handymanImage}
-        className="handyman-card-image"
-      />
-      <div className="handyman-card-details">
-        <div className="handyman-name">János</div>
-        <div className="handyman-expertise">
-          <div className="handyman-skills">
-            Experienced gardener,<br/>expert plumber
-          </div>
-          <div className="handyman-rate">⭐ 4.91 (484)</div>
-        </div>
-        <div className="handyman-address">🏙 Budapest IX. district</div>
-        <div className="handyman-available">📅 Available: Oct 23-26</div>
-        <div className="handyman-wage">💲 17 - 29 / hour</div>
-      </div>
-    </div>
-  );
+export default function HandymanCard({lastName, firstName, hourlyWage, skills}) {
+    return (<div className="handyman-card">
+            <img
+                src={handymanImage}
+                className="handyman-card-image"
+             alt={"handyman-janos.jpg"}/>
+            <div className="handyman-card-details">
+                <div className="handyman-name">{lastName + " " + firstName}</div>
+                <div className="handyman-expertise">
+                    {skills ? skills.map((skill,i) => {
+                        return (<div key={i}>
+                                {skill}
+                            </div>);
+                    }) : ("")}
+                    {/*<div className="handyman-rate">⭐ 4.91 (484)</div>*/}
+                </div>
+                <div className="handyman-wage">💲 {hourlyWage} / hour</div>
+            </div>
+        </div>);
 }
--- TaskTigerFrontEnd/src/Pages/HomePage/HomePage.jsx
+++ TaskTigerFrontEnd/src/Pages/HomePage/HomePage.jsx
@@ -1,88 +1,123 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
 import HandymanCard from "../../Components/HandymanCard/HandymanCard";
 import Title from "../../Components/Title";
 import "./HomePage.css";
+import Loading from "../../Components/Loading/Loading.jsx";
+
+const fetchUsers = (signal) => {
+    return fetch("api/users/tasker/all", {signal}).then((res) => res.json());
+};
 
 export default function HomePage() {
-  const navigate = useNavigate();
+    const [users, setUsers] = useState(null);
+    const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    navigate("/myprofile");
-  }
+    useEffect(() => {
+        const controller = new AbortController();
 
-  return (
-    <div className="home-page">
-     <Title titleClass={"title"} subTitleClass={"sub-title"}/>
-      <div className="home-container">
-        <div className="home-description">
-          <div className="description-text">
-            <div className="description-text-title">
-              Everyday life made easier
-            </div>
-            When life gets busy, you don't have to tackle it alone.
-            <br />
-            Get time back for what you love without breaking the bank.
-            <br />
-            <br />
-            - Choose your Tasker by reviews, skills, and price
-            <br />
-            - Schedule when it works for you — as early as today
-            <br />
-            - Chat, pay, tip, and review all through one platform
-            <br />
-          </div>
+        fetchUsers(controller.signal)
+            .then((users) => {
+                setUsers(users);
+            })
+            .catch((error) => {
+                if (error.name !== "AbortError") {
+                    setUsers(null);
+                    throw error;
+                }
+            });
+        return () => controller.abort();
+    }, []);
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        navigate("/myprofile");
+    }
 
-          <div className="handyman-cards">
-            <HandymanCard />
-            <HandymanCard />
-            <HandymanCard />
-            <HandymanCard />
-          </div>
-        </div>
-        <div className="home-form">
-          <div className="form-title">Register here</div>
-          <form className="home-form-form">
-            <input name="name" placeholder="Your name"></input>
-            <input name="phone" placeholder="Your phone number"></input>
-            <input name="dob" type={"date"} id="dob"></input>
-            <div className="isTasker-checkbox">
-              <input name="isTasker" type={"checkbox"} id="isTasker"></input>
-              <label htmlFor="isTasker" id="isTasker-label">
-                I want to be a Tasker!
-                <br />
-                <span style={{ fontSize: "0.7rem", color: "rgb(68,68,68)" }}>
+    return (
+        <div className="home-page">
+            {users ? (
+                <>
+                    <Title titleClass={"title"} subTitleClass={"sub-title"}/>
+                    <div className="home-container">
+                        <div className="home-description">
+                            <div className="description-text">
+                                <div className="description-text-title">
+                                    Everyday life made easier
+                                </div>
+                                When life gets busy, you don't have to tackle it alone.
+                                <br/>
+                                Get time back for what you love without breaking the bank.
+                                <br/>
+                                <br/>
+                                - Choose your Tasker by reviews, skills, and price
+                                <br/>
+                                - Schedule when it works for you — as early as today
+                                <br/>
+                                - Chat, pay, tip, and review all through one platform
+                                <br/>
+                            </div>
+                            <div>
+                                {users.map((currentUser) => {
+                                    return (
+                                        <div className="handyman-cards"
+                                             key={currentUser.id}
+                                        >
+                                            <HandymanCard
+                                                lastName={currentUser.lastName}
+                                                firstName={currentUser.firstName}
+                                                hourlyWage={currentUser?.taskerInfo?.hourlyWage}
+                                                skills={currentUser?.taskerInfo?.skills}
+                                            />
+                                        </div>
+                                    );
+                                })};
+                            </div>
+                        </div>
+                        <div className="home-form">
+                            <div className="form-title">Register here</div>
+                            <form className="home-form-form">
+                                <input name="name" placeholder="Your name"></input>
+                                <input name="phone" placeholder="Your phone number"></input>
+                                <input name="dob" type={"date"} id="dob"></input>
+                                <div className="isTasker-checkbox">
+                                    <input name="isTasker" type={"checkbox"} id="isTasker"></input>
+                                    <label htmlFor="isTasker" id="isTasker-label">
+                                        I want to be a Tasker!
+                                        <br/>
+                                        <span style={{fontSize: "0.7rem", color: "rgb(68,68,68)"}}>
                   This way you will be able to receive tasks and earn money! You
                   can specify your expertise on your profile page.
                 </span>
-              </label>
-            </div>
-            <input
-              name="email"
-              type={"email"}
-              placeholder="Your email address"
-            ></input>
-            <input
-              name="password"
-              type={"password"}
-              placeholder="Your password"
-            ></input>
-            <input
-              name="password-again"
-              type={"password"}
-              placeholder="Your password again"
-            ></input>
-            <input
-              name="submit"
-              type={"submit"}
-              id="submit-btn"
-              value={"Register"}
-              onClick={(e) => handleSubmit(e)}
-            ></input>
-          </form>
+                                    </label>
+                                </div>
+                                <input
+                                    name="email"
+                                    type={"email"}
+                                    placeholder="Your email address"
+                                ></input>
+                                <input
+                                    name="password"
+                                    type={"password"}
+                                    placeholder="Your password"
+                                ></input>
+                                <input
+                                    name="password-again"
+                                    type={"password"}
+                                    placeholder="Your password again"
+                                ></input>
+                                <input
+                                    name="submit"
+                                    type={"submit"}
+                                    id="submit-btn"
+                                    value={"Register"}
+                                    onClick={(e) => handleSubmit(e)}
+                                ></input>
+                            </form>
+                        </div>
+                    </div>
+                </>
+            ) : (
+                <Loading/>)}
         </div>
-      </div>
-    </div>
-  );
+    );
 }
