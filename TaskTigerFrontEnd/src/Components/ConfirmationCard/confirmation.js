9e2b1321a4ed19cae7bcc70d283a74e28cfb5e94
miz092 <mihalyfi.zoltan@gmail.com>
2023-03-03 01:34:56 +0100

    make small changes to confirmationcard

---
+++ TaskTigerFrontEnd/src/Components/ConfirmationCard/confirmation.js
@@ -0,0 +1,17 @@
+async function postReservation(data) {
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+
+  const response = await fetch(
+    "http://localhost:8080/api/reservation",
+    options
+  );
+  const responseData = await response.json();
+
+  return responseData;
+}
