cd2df592d1ec329b71b29ef7f1c2531c62e274fe
miz092 <mihalyfi.zoltan@gmail.com>
2023-03-03 01:35:21 +0100

    make small changes to confirmationcard

--- TaskTigerFrontEnd/src/Components/ConfirmationCard/ConfirmationCard.jsx
+++ TaskTigerFrontEnd/src/Components/ConfirmationCard/ConfirmationCard.jsx
@@ -1,82 +1,93 @@
 import React from "react";
 import "./ConfirmationCard.css";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { postReservation } from "./confirmation";
 
-export default function ConfirmationCard() {
-    const navigate = useNavigate();
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        navigate("/thankyou");
-    }
-    return (
-        <div className="confirmation-card">
-            <div className="confirmation-card-title">Confirmation Details:</div>
-            <div className="confirmation-card-details">
-                <div className="confirmation-details-line">
-                    <div className="confirmation-details-nameTag">Client:</div>
-                    <div className="confirmation-details-parameter">Jane Foster</div>
-                </div>
-                <div className="confirmation-details-line">
-                    <div className="confirmation-details-nameTag">Job:</div>
-                    <div className="confirmation-details-parameter">Dog walking</div>
-                </div>
-                <div className="confirmation-details-line">
-                    <div className="confirmation-details-nameTag">Duration:</div>
-                    <div className="confirmation-details-parameter">2 hour</div>
-                </div>
-                <div className="confirmation-details-line">
-                    <div className="confirmation-details-nameTag">Address:</div>
-                    <div className="confirmation-details-parameter">Budapest, Népfürdő u. 21f, 1138</div>
-                </div>
-                <div className="confirmation-details-line">
-                    <div className="confirmation-details-nameTag">Description:</div>
-                    <div className="confirmation-details-parameter">Please walk 5 km.</div>
-                </div>
-                <div className="confirmation-details-line">
-                    <div className="confirmation-details-nameTag">Message:</div>
-                    <div className="confirmation-details-parameter">Call me when you're here at the door. 125
-                        doorbell!
-                    </div>
-                </div>
-                <div className="confirmation-details-selectedPeopleBox">
-                    <div className="confirmation-details-selectedPeopleBox-title">
-                        <div className="confirmation-details-nameTag">Selected people</div>
-                        <div className="confirmation-details-nameTag">Request status</div>
-                    </div>
+export default function ConfirmationCard({
+  client,
+  job,
+  duration,
+  address,
+  description,
+  message,
+  tasker,
+  workType,
+}) {
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-                    <div className="confirmation-details-taskerContainer">
-                        <div className="confirmation-details-taskerTagAndName">
-                            <div className="confirmation-details-taskerTag">Tasker:</div>
-                            <div className="confirmation-details-taskerName">Derrick</div>
-                        </div>
-                        <div> sending request</div>
-                    </div>
-                    <hr/>
-                    <div className="confirmation-details-line-price">
-                        <div className="confirmation-details-line-price">
-                            <div>
-                                2
-                            </div>
-                            <div>x</div>
-                            <div>
-                                $30.58
-                            </div>
-                        </div>
-                        <div className>
-                            $61.97
-                        </div>
-                    </div>
-                    <div className="confirmation-details-line-total">
-                        <div className="confirmation-details-nameTag">Total (USD):</div>
-                        <div className="confirmation-details-line-total-priceValue">$61.97</div>
-                    </div>
-                </div>
-                <button className="buttonSubmit"
-                        type={"submit"}
-                        onClick={(e) => handleSubmit(e)}
-                >Send request to Tasker(s)
-                </button>
-            </div>
+    navigate("/thankyou");
+  };
+  return (
+    <div className="confirmation-card">
+      <div className="confirmation-card-title">Confirmation Details:</div>
+      <div className="confirmation-card-details">
+        <div className="confirmation-details-line">
+          <div className="confirmation-details-nameTag">Client:</div>
+          <div className="confirmation-details-parameter">
+            {client.userName}
+          </div>
         </div>
-    );
+        <div className="confirmation-details-line">
+          <div className="confirmation-details-nameTag">Job:</div>
+          <div className="confirmation-details-parameter">{job}</div>
+        </div>
+        <div className="confirmation-details-line">
+          <div className="confirmation-details-nameTag">Duration:</div>
+          <div className="confirmation-details-parameter">{duration}</div>
+        </div>
+        <div className="confirmation-details-line">
+          <div className="confirmation-details-nameTag">Address:</div>
+          <div className="confirmation-details-parameter">{address}</div>
+        </div>
+        <div className="confirmation-details-line">
+          <div className="confirmation-details-nameTag">Description:</div>
+          <div className="confirmation-details-parameter">{description}</div>
+        </div>
+        <div className="confirmation-details-line">
+          <div className="confirmation-details-nameTag">Message:</div>
+          <div className="confirmation-details-parameter">{message}</div>
+        </div>
+        <div className="confirmation-details-selectedPeopleBox">
+          <div className="confirmation-details-selectedPeopleBox-title">
+            <div className="confirmation-details-nameTag">Selected people</div>
+            <div className="confirmation-details-nameTag">Request status</div>
+          </div>
+
+          <div className="confirmation-details-taskerContainer">
+            <div className="confirmation-details-taskerTagAndName">
+              <div className="confirmation-details-taskerTag">Tasker:</div>
+              <div className="confirmation-details-taskerName">
+                {tasker.userName}
+              </div>
+            </div>
+            <div> sending request</div>
+          </div>
+          <hr />
+          <div className="confirmation-details-line-price">
+            <div className="confirmation-details-line-price">
+              <div>2</div>
+              <div>x</div>
+              <div>$30.58</div>
+            </div>
+            <div className>$61.97</div>
+          </div>
+          <div className="confirmation-details-line-total">
+            <div className="confirmation-details-nameTag">Total (USD):</div>
+            <div className="confirmation-details-line-total-priceValue">
+              $61.97
+            </div>
+          </div>
+        </div>
+        <button
+          className="buttonSubmit"
+          type={"submit"}
+          onClick={(e) => handleSubmit(e)}
+        >
+          Send request to Tasker(s)
+        </button>
+      </div>
+    </div>
+  );
 }
