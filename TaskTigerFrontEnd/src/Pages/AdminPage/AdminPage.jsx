6fd21df6a635379853cf2f62a83d20b36eade8ce
miz092 <mihalyfi.zoltan@gmail.com>
2023-03-16 18:40:19 +0100

    add admin page and usercards to the page. implement page choice based on adminness at sign in and admin can delete users

--- TaskTigerFrontEnd/src/Pages/AdminPage/AdminPage.jsx
+++ TaskTigerFrontEnd/src/Pages/AdminPage/AdminPage.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
-import "./MyProfile.css";
+import "./AdminPage.css";
 import profileImage from "../../Images/handyman-janos.jpg";
-import ReservationCard from "../../Components/ReservationCard/ReservationCard";
 import { useNavigate } from "react-router-dom";
+import UserCard from "../../Components/UserCard/UserCard";
 
-export default function MyProfile() {
+function AdminPage() {
   const [user, setUser] = useState(null);
+  const [users, setUsers] = useState(null);
+  const [deleted, setDeleted] = useState(false);
+
   const isLoggedIn =
     localStorage.getItem("token") !== null &&
     localStorage.getItem("token") !== "null";
@@ -27,7 +30,6 @@ export default function MyProfile() {
         const user = await res.json();
         console.log(user);
         setUser(user);
-        user.role.name === "ADMIN" ? navigate("/adminpage") : null;
       } catch (error) {
         console.log(error);
       }
@@ -35,6 +37,15 @@ export default function MyProfile() {
 
     fetchData();
   }, []);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("/api/users/all");
+      const data = await response.json();
+      setUsers(data);
+    }
+
+    fetchData();
+  }, [deleted]);
 
   const getAge = (birthDate) => {
     const thisDate = new Date();
@@ -45,49 +56,53 @@ export default function MyProfile() {
   };
 
   return user ? (
-    <div className="myprofile-page">
-      <div className="myprofile-name">
+    <div className="adminPage-page">
+      <div className="adminPage-name">
         {user.firstName + " " + user.lastName}
       </div>
-      <div className="myprofile-details">
-        <div className="myprofile-user-details">
-          <div className="myprofile-image-div">
-            <img src={profileImage} className="myprofile-image" />
+      <div className="adminPage-details">
+        <div className="adminPage-user-details">
+          <div className="adminPage-image-div">
+            <img src={profileImage} className="adminPage-image" />
           </div>
-          <div className="myprofile-user-info">
-            <div className="myprofile-user-info-title">
-              {user.tasker ? "TASKER" : "CLIENT"}
-            </div>
-            <div className="myprofile-user-info-text">
+          <div className="adminPage-user-info">
+            <div className="adminPage-user-info-title">ADMIN</div>
+            <div className="adminPage-user-info-text">
               <ul>
                 <li>{user.username}</li>
                 <li>{getAge(user.dob)}</li>
                 <li>{user.gender}</li>
                 <li>
-                  member since {new Date(user.registrationDate).getFullYear()}
+                  Admin since {new Date(user.registrationDate).getFullYear()}
                 </li>
-                <li>tasks given out: 14</li>
               </ul>
             </div>
           </div>
-          <div className="myprofile-user-intro">
-            <div className="myprofile-user-intro-title">ABOUT ME</div>
-            <div className="myprofile-user-intro-text">
+          <div className="adminPage-user-intro">
+            <div className="adminPage-user-intro-title">ABOUT ME</div>
+            <div className="adminPage-user-intro-text">
               {user.shortIntroduction}
             </div>
           </div>
         </div>
-        <div className="myprofile-reservation-container">
-          <div className="myprofile-reservation-container-title">
-            Your upcoming tasks 📅
-          </div>
-          <ReservationCard />
-          <ReservationCard />
-          <ReservationCard />
-          <ReservationCard />
-          <ReservationCard />
+        <div className="adminPage-reservation-container">
+          <div className="adminPage-reservation-container-title">All users</div>
+          {users ? (
+            users.map((user, i) => (
+              <UserCard
+                key={i}
+                state={deleted}
+                setState={setDeleted}
+                user={user}
+              ></UserCard>
+            ))
+          ) : (
+            <></>
+          )}
         </div>
       </div>
     </div>
   ) : null;
 }
+
+export default AdminPage;
